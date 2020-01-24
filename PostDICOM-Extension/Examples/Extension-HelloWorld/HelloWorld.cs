daefe585235230ea2012e65182536498c5e0a9dd
EMRE <EMRE@DESKTOP-V51968C>
2020-01-24 15:54:03 +0300

    HelloWorld template started.

--- PostDICOM-Extension/Examples/Extension-HelloWorld/HelloWorld.cs
+++ PostDICOM-Extension/Examples/Extension-HelloWorld/HelloWorld.cs
@@ -1,8 +1,21 @@
-﻿using System;
+﻿using PostDicomPlugin;
+using PostDicomPlugin.Objects;
+using System;
 
 namespace Extension_HelloWorld
 {
-    public class HelloWorld
+    public class HelloWorld : pdPlugin
     {
+        public override void Initialize()
+        {
+            base.Initialize();
+            PluginId = Guid.Parse("38145f18-3ea8-11ea-b77f-2e728ce88125");//Create new Guid for your project
+        }
+
+        public override void MessageReceivedFromUI(object rmo, pdSession session, string content)
+        {
+            base.MessageReceivedFromUI(rmo, session, content);
+            SendMessageToUI(rmo, "Hello from server. ClientMessage: " + content);
+        }
     }
 }
