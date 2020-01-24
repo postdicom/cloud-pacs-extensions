256ff6e27b71b4d48ad380ff5b9989e546ed5504
EMRE <EMRE@DESKTOP-V51968C>
2020-01-24 13:57:43 +0300

    Extension template started.

--- PostDICOM-Extension/Template/Extension-Template/Extension.cs
+++ PostDICOM-Extension/Template/Extension-Template/Extension.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using PostDicomPlugin;
+using PostDicomPlugin.Objects;
+using System;
 
 namespace Extension_Template
 {
-    public class Extension
+    public class Extension : pdPlugin
     {
+        public override void Initialize()
+        {
+            base.Initialize();
+            PluginId = Guid.Parse("8f974cb1-8415-4ac2-90eb-ebb081fba0bd");//Create new Guid for your project
+        }
+
+        public override void MessageReceivedFromUI(object rmo, pdSession session, string content)
+        {
+            base.MessageReceivedFromUI(rmo, session, content);
+        }
     }
 }
